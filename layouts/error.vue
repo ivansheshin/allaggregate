0b7e2dc6d577a7f79ff384c758863284b4ce1a0a
Ivan Sheshin <ivashesh@gmail.com>
2023-01-19 17:26:12 +0300

    добавил компонент ошибки

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content">
+    <section>
+      <img class="capybara" src="../assets/capybara.jpg">
+      <section class="actions">
+        <h2 class="error-title">
+          Error epta
+        </h2>
+        <nuxt-link to="/">
+          Гоу ту хоум
+        </nuxt-link>
+      </section>
+    </section>
+  </div>
+</template>
+<style scoped>
+.error-title {
+  color: brown;
+}
+</style>
