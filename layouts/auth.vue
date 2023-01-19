f6740c8a6c2965ade6088943642cab6081fc58ec
Ivan Sheshin <ivashesh@gmail.com>
2023-01-19 18:24:31 +0300

    добавил лейаут для авторизации

---
+++ layouts/auth.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+    <section>
+      <img class="capybara" src="../assets/capybara.jpg">
+      <div class="authpage">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.authpage {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
