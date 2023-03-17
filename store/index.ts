40a1b3564ef5f9be5c473187bf6577606be6712b
ivansheshin <ivashesh@gmail.com>
2023-03-17 23:53:03 +0300

    updated store

--- store/index.ts
+++ store/index.ts
@@ -12,7 +12,12 @@ export const getters: GetterTree<RootState, RootState> = {
 
 export const mutations: MutationTree<RootState> = {
   SET_USER_ID: (state, UID: string) => (state.userIDInfo = UID),
-  RESET_USER_ID: state => (state.userIDInfo = '')
+  RESET_USER_ID: state => (state.userIDInfo = ''),
+  INIT_CART_FROM_LOCALSTORAGE (state) {
+    const UID = localStorage.getItem('UID')
+
+    state.userIDInfo = UID || ''
+  }
 }
 
 export const actions: ActionTree<RootState, RootState> = {
@@ -21,5 +26,8 @@ export const actions: ActionTree<RootState, RootState> = {
   },
   resetUserId ({ commit }) {
     commit('RESET_USER_ID')
+  },
+  nuxtClientInit ({ commit }) {
+    commit('INIT_CART_FROM_LOCALSTORAGE')
   }
 }
